9260bba7bf4647661872bf9117f4476fb93d8403
june <54416373+jhunbaclaan@users.noreply.github.com>
2023-03-16 10:31:27 -1000

    Create Pet.java
    practicing class diagram thing idk

---
+++ week10/Pet.java
@@ -0,0 +1,17 @@
+package week10;
+/*
+ Pet
+ - name : String
+ - type : String
+ - age : int
+ + setName(n : String) : void
+ + setType (t : String) : void
+ + setAge (a : int) : void
+ + getName() : String
+ + getType() : String
+ + getAge() : int
+ */
+public class Pet {
+    private String name, type;
+    private int age;
+}
